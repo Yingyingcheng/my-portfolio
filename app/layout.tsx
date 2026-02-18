91ba777344d58659ef16e13965b177a1d57988b6
Yingyingcheng <doris82717@gmail.com>
2026-02-17 21:38:44 -0800

    Add favicon in layout.tsx.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google"; // Only keep Montserrat
 import "./globals.css";
 
-// 1. Configure only the font you want to use
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700", "900"],
@@ -12,6 +11,26 @@ const montserrat = Montserrat({
 export const metadata: Metadata = {
   title: "Doris Cheng",
   description: "Built with Next.js and Tailwind CSS",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
